0187dae4c4b0c93116496775591ceac7111a4b0f
unknown <suhairi81@gmail.com>
2015-11-12 11:39:36 +0800

    2015-11-12 1

--- resources/views/members/admin/pengguna.blade.php
+++ resources/views/members/admin/pengguna.blade.php
@@ -5,13 +5,12 @@
     <div class="row">
         <div class="col-xs-4">
 
-            <div class="panel panel-info">
-                <h3>Kemaskini Pengguna</h3>
-
+            <div class="panel panel-primary">
+                <div class="panel-heading"><h4>Kemaskini Pengguna</h4></div>
                 <div class="panel-body">
 
                     {!! Form::model($user, ['route' => ['members.admin.profile.update', $user->username], 'method' => 'PATCH']) !!}
-                    @include('forms._profile', ['submitButton' => 'Kemaskini Pengguna'])
+                        @include('forms._profile', ['submitButton' => 'Kemaskini Pengguna'])
                     {!! Form::close() !!}
 
                 </div>
